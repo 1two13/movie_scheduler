53cd34e1d451f4a242d64cdf79b83dc69a4775e3
Tory <88531407+1two13@users.noreply.github.com>
2022-06-29 12:39:43 +0900

    feature: 개봉 예정작 fetch로 받아오기 (1페이지만 받아온 상황)

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-function nowMovie() {
-  const key = "ec0349e18bd5a15f1fcf54bad2b7f06c";
+const key = "ec0349e18bd5a15f1fcf54bad2b7f06c";
+const movieBox = document.getElementById("movieBox");
 
+function nowMovie() {
   // 어제 날짜 구하기
   let today = new Date();
   let yesterday = new Date(today.setDate(today.getDate() - 1));
@@ -15,8 +16,6 @@ function nowMovie() {
 
   // 일별 박스오피스 API
   const dayApi = `https://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=${key}&targetDt=${yesterday}`;
-
-  const movieBox = document.getElementById("movieBox");
 
   const reqPromise = fetch(dayApi, {
     headers: { Accept: "application/json" },
@@ -39,6 +38,41 @@ function nowMovie() {
                 <div>누적 관객수 ${Math.round(movie.audiAcc / 10000)}만 명 | 
                   ${movie.openDt} 개봉
                 </div>
+              </div>
+            `
+        )
+        .join("");
+    })
+    .catch((err) => alert(err));
+}
+
+function comingMovie() {
+  let year = new Date().getFullYear();
+  let curPage = 1;
+
+  // 개봉 예정작 API
+  let comingApi = `https://kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieList.json?key=${key}&openStartDt=${year}&itemPerPage=100&curPage=${curPage}`;
+
+  const reqPromise = fetch(comingApi, {
+    headers: { Accept: "application/json" },
+    method: "GET",
+  });
+
+  reqPromise
+    .then((res) => {
+      if (res.status >= 200 && res.status < 300) return res.json();
+      else return Promise.reject(new Error(`Got status ${res.status}`));
+    })
+    .then((movieBoxEl) => {
+      movieBox.innerHTML = movieBoxEl.movieListResult.movieList
+        .filter((upcoming) => upcoming.prdtStatNm === "개봉예정")
+        .sort((a, b) => a.openDt - b.openDt)
+        .map(
+          (movie) =>
+            `
+              <div>
+                <div>${movie.movieNm}</div>
+                <div>${movie.openDt} 개봉 예정</div>
               </div>
             `
         )
